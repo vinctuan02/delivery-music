dd22bc95748b7b4ba336a0856bf6ef5979cedbb8
vinctuan02 <vinctuan02@gmail.com>
2025-05-21 10:33:43 +0700

    feat: add eslint config and upload module

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist/**', 'node_modules/**'],
+  rules: {
+    // tùy chỉnh nếu cần
+  },
+};
